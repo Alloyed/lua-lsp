7f818161b751fe7ec8ee727889a95466e4268694
Kyle McLamb <kjmclamb@gmail.com>
2017-09-16 21:43:18 -0400

    Use only one luarocks version in pre-push
    That was kind of tedious :p

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -11,17 +11,10 @@ if [ -z "$commits" ]; then
     exit 0
 fi
 
-# make sure project installs and loads correctly
-for POST in "5.1" "5.2" ""; do
-	DASH="$POST"
-	if [ "$POST" != "" ]; then
-		DASH="-$POST"
-	fi
-	eval $("luarocks$DASH" path --tree="/tmp/lua$POST")
-	"luarocks$DASH" make --tree="/tmp/lua$POST"
-	if [ "$POST" == "5.1" ]; then
-		luajit -l lua-lsp.loop -e "os.exit(0)"
-	fi
-	"lua$POST" -l lua-lsp.loop -e "os.exit(0)"
-	rm -rv "/tmp/lua$POST"
-done
+LUAROCKS="luarocks-5.1"
+LUA="luajit"
+TREE="/tmp/lsptest"
+eval $($LUAROCKS path --tree="$TREE")
+$LUAROCKS make --tree="$TREE"
+$LUA -l lua-lsp.loop -e "os.exit(0)"
+rm -rv "$TREE"
